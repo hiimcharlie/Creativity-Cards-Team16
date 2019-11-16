367c78def5cecdaa29f23c88ee181b7971cb8365
AdrianTofan <adrianvasile.tofan@gmail.com>
2019-11-16 19:57:09 +0000

    Added the Unity project to the repository.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Controls controller;
+    float horizontalMove;
+    bool jump = false;
+    public float runSpeed = 100f;
+   
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetKeyDown(KeyCode.Space))
+            jump = true;
+
+        
+
+        
+    }
+
+   void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
